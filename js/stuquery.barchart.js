1d5c7c4869584bba9c807fae792ca32614ddca94
Stuart <gravatar@strudel.org.uk>
2017-03-05 00:22:06 +0000

    Refactoring and bug fixing
    * Bins are now anchor elements so that they can be tabbed through with
    the keyboard
    * We only redraw the graph if the ranges have changed (to allow heights
    of bins to animate)
    * Focussing on a bar triggers the barover event

--- js/stuquery.barchart.js
+++ js/stuquery.barchart.js
@@ -12,34 +12,37 @@
 
 	function BarChart(target,attr){
 
-		var ver = "0.3";
+		var ver = "0.4";
 		this.target = target;
 		if(S(this.target).length == 0) return {};
 		this.attr = attr || {};
-		this.bins = [];
+		this.bins;
 		this.events = {resize:""};
 		this.attr.units = (typeof this.attr.units==="undefined") ? "" : this.attr.units;
 		this.attr.formatKey = (typeof this.attr.formatKey==="undefined") ? function(key){ return key; } : this.attr.formatKey;
 		this.parent = (typeof this.attr.parent==="undefined") ? this : this.attr.parent;
 	
-		//if(typeof this.attr.max==="undefined") this.attr.max = this;
-		this.max = this.attr.max;
-		this.min = this.attr.min;
-		this.inc = this.attr.inc;
+		this.drawn = false;
+		this.max = this.attr.max || undefined;
+		this.min = this.attr.min || undefined;
+		this.inc = this.attr.inc || undefined;
 		this.mintick = this.attr.mintick || 3;
-		var _obj = this;
-		this.bin = (typeof this.attr.bin==="function") ? function(v){ return this.attr.bin(v); } : function(v){ return Math.floor((v - this.min)/this.inc); };
-		this.version = function(){ return ver; }	
+		this.bin = (typeof this.attr.bin==="function") ? this.attr.bin : function(v){ return (this.typ==="string" ? this.fields[v] : Math.floor((v - this.min)/this.inc)); };
+		this.version = function(){ return ver; }
 		return this;
 	}
 
 	BarChart.prototype.isSelected = function(v){
 		b = this.bin(v);
+		// If there is no value, use the first bin
+		if(typeof v==="undefined") b = 0;
+		if(b >= this.nbins) b = this.nbins-1;
+		if(b < 0) b = 0;
 		return (typeof this.bins[b].selected==="boolean") ? this.bins[b].selected : true;
 	}
 
 	BarChart.prototype.select = function(r){
-		for(var b = 0; b < this.bins.length; b++){
+		for(var b = 0; b < this.nbins; b++){
 			if(r=="all") this.bins[b].selected = true;
 			else this.bins[b].selected = false;
 			if(this.bins[b].selected) S('#'+this.bins[b].id).removeClass('deselected');
@@ -48,106 +51,129 @@
 		return this;
 	}
 	BarChart.prototype.toggleBin = function(b){
-		key = this.bins[b].key;
 		this.bins[b].selected = !this.bins[b].selected;
 		S('#'+this.bins[b].id+'').toggleClass('deselected');
 		return this;
 	}
-	
-	BarChart.prototype.setBins = function(data,attr){
-		if(!attr) attr = {};
+	// A function to provide the data
+	// Our assumption is that the bins will be the same as any previous data
+	// If you need to update the scales, call setBins afterwards
+	BarChart.prototype.setData = function(data){
 		if(data && typeof data.length==="number") this.data = data;
-		else{
-			if(data) attr = data;
-		}
+		return this;
+	}
+	// A function to set/change any scaling
+	BarChart.prototype.setBins = function(attr){
+		if(!attr) attr = {};
+
+		var calc = false;
+
 		var typ = (this.data.length > 0 && typeof this.data[0][0]==="string") ? "string" : "number";
-		if(!this.bins) this.bins = [];
-		if(!attr.inc) attr.inc = this.inc;
-		if(!attr.max) attr.max = this.max;
-		if(!attr.min) attr.min = this.min;
-		if(!attr.mintick) attr.mintick = this.mintick;
+		if(this.typ && typ!=this.typ) calc = true;
+		// Update our record of the type of binning
+		this.typ = typ;
+
+		// If we haven't been provided a setting, use a previously set value
+		if(typeof attr.inc==="undefined") attr.inc = this.inc;
+		if(typeof attr.max==="undefined") attr.max = this.max;
+		if(typeof attr.min==="undefined") attr.min = this.min;
+		if(typeof attr.mintick==="undefined") attr.mintick = this.mintick;
+
+		// See if anything has changed
+		if(attr.inc!=this.inc) calc = true;
+		if(attr.min!=this.min) calc = true;
+		if(attr.max!=this.max) calc = true;
+
+		// Do we have any settings missing at this point?
+		if(this.typ=="number"){
+			if(typeof attr.inc!=="number") calc = true;
+			if(typeof attr.min!==typ) calc = true;
+			if(typeof attr.max!==typ) calc = true;
+			if(typeof attr.mintick!=="number") calc = true;
+		}
+
+		if(!this.bins) calc = true;
+		
 		if(typ==="string"){
+			// We are in a category-based binning regime
 			var f = 0;
-			var fields = {};
-			// Create empty bins
-			for(var r = 0; r < this.data.length; r++){
-				if(!fields[this.data[r][0]]){
-					fields[this.data[r][0]] = f;
-					f++;
+			if(calc){
+				this.fields = {};
+				this.bins = {};
+				// Create empty bins
+				for(var r = 0; r < this.data.length; r++){
+					if(!this.fields[this.data[r][0]]){
+						this.fields[this.data[r][0]] = f;
+						f++;
+					}
+					b = this.fields[this.data[r][0]];
+					if(!this.bins[b]) this.bins[b] = {'selected':true};
+					//this.bins[b].value = 0;
+					if(this.bins[b].key != this.data[r][0]){
+						this.bins[b].key = this.data[r][0];
+						this.drawn = false;
+					}
 				}
-				b = fields[this.data[r][0]];
-				if(!this.bins[b]) this.bins[b] = {'selected':true};
-				this.bins[b].value = 0;
-				if(this.bins[b].key != this.data[r][0]){
-					this.bins[b].key = this.data[r][0];
-					this.drawn = false;
-				}
+				this.nbins = f;
 			}
-			f = 0;
-			// Populate bins
-			for(var r = 0; r < this.data.length; r++){
-				if(!fields[this.data[r][0]]){
-					fields[this.data[r][0]] = f;
-					f++;
-				}
-				b = fields[this.data[r][0]];
-				this.bins[b].value += this.data[r][1];
-			}
+			// Empty bins
+			for(var b in this.bins) this.bins[b].value = 0;
 		}else{
-			// Find range of data
-			var s = 1e100;
-			var e = -1e100;
-			var binning = {};
-			for(var o = 0; o < this.data.length; o++){
-				if(this.data[o][0] < s) s = this.data[o][0];
-				if(this.data[o][0] > e) e = this.data[o][0];
-			}
-			// Get binning of data
-			if(attr.inc){
-				binning.max = (attr.max) ? attr.max : e;
-				binning.min = (attr.min) ? attr.min : s;
-				binning.inc = attr.inc;
+			// We are in a number based binning regime
+			if(calc){
+				// Find range of data
+				var s = 1e100;
+				var e = -1e100;
+				var binning = {};
+				for(var o = 0; o < this.data.length; o++){
+					if(this.data[o][0] < s) s = this.data[o][0];
+					if(this.data[o][0] > e) e = this.data[o][0];
+				}
+				// Get binning of data
+				if(attr.inc){
+					binning.max = (attr.max) ? attr.max : e;
+					binning.min = (attr.min) ? attr.min : s;
+					binning.inc = attr.inc;
+				}else{
+					binning = this.getGrid(s,e,attr.mintick);
+				}
+				binning.range = binning.max - binning.min;
+				binning.bins = Math.ceil(binning.range/binning.inc)+1;
+
 				this.drawn = false;
-			}else{
-				binning = this.getGrid(s,e,attr.mintick);
+				this.bins = [];
+
+				// Set main value		
+				this.max = binning.max;
+				this.min = binning.min;
+				this.inc = binning.inc;
+				this.range = binning.range;
+				this.nbins = binning.bins;
 			}
-			binning.range = binning.max - binning.min;
-			binning.bins = Math.ceil(binning.range/binning.inc)+1;
-			if(binning.max != this.max || binning.min != this.min || binning.inc != this.inc) this.drawn = false;
-			if(this.inc && binning.inc != this.inc) this.drawn = false;
-			if(binning.inc != this.inc) this.bins = [];
-
-			// Set main value		
-			this.max = binning.max;
-			this.min = binning.min;
-			this.inc = binning.inc;
-
+			if(typeof this.nbins!=="number") this.nbins = Math.ceil(this.range/this.inc);
 			// Empty the bins
-			for(var b = 0 ; b < binning.bins ; b++){
+			for(var b = 0 ; b <= this.nbins ; b++){
 				if(!this.bins[b]) this.bins[b] = {'selected':true};
 				this.bins[b].value = 0;
-				this.bins[b].key = ''+(binning.min + b*binning.inc);
+				this.bins[b].key = ''+(this.min + b*this.inc);
 			}
-
-			// Populate bins
-			for(var r = 0; r < this.data.length; r++){
-				b = this.bin(this.data[r][0]);
-				this.bins[b].value += this.data[r][1];
-			}
+		}
+		// Populate bins
+		for(var r = 0; r < this.data.length; r++){
+			b = this.bin(this.data[r][0]);
+			this.bins[b].value += this.data[r][1];
 		}
 		return this;
 	}
-
 	BarChart.prototype.draw = function(){
 
 		var id = this.target.substr(1);
 		if(!this.target || !this.bins) return this;
 
 		var mx = 0;
-		var nbins = this.bins.length;
 	
-		if(nbins > 0){
-
+		if(this.nbins > 0){
+		
 			if(S(this.target+' .label').length == 0) S(this.target).append('<span class="label">label</span>');
 			if(!this.lineheight) this.lineheight = S(this.target).find('.label')[0].offsetHeight;
 			// Set the height of the graph
@@ -155,11 +181,9 @@
 			h = this.height-this.lineheight;
 
 			// Find the peak value
-			for(var b = 0; b < this.bins.length; b++){
+			for(var b = 0; b < this.nbins; b++){
 				if(this.bins[b].value > mx) mx = this.bins[b].value;
 			}
-
-			if(typeof this.drawn==="undefined") this.drawn = false;
 
 			// Build the basic graph structure
 			if(!this.drawn) S(this.target).addClass('barchart').html('<div class="grid" style="height:'+(h)+'px;"></div><table style="height:'+this.height+'px"><tr style="vertical-align:bottom;"></tr></table><div style="clear:both;"></div>');
@@ -172,21 +196,19 @@
 
 			var maketable = (S(this.target+' table td').length == 0);
 			output = "";
-	
-			for(var b = 0; b < this.bins.length; b++){
-				var key = this.bins[b].key;
-				var hbar = (mx > 0 ? (h*this.bins[b].value/mx).toFixed(1) : 0);
-				var ha = Math.round(h-hbar);
-				var hb = h-ha;
-				if(hb < 1){
-					ha--;
-				}
 
-				var idbar = id+'-bar-'+key.replace(/ /g,'-');
+			var key,hbar,ha,hb,idbar, p;	
+			for(var b = 0; b < this.nbins; b++){
+				key = this.bins[b].key;
+				hbar = (mx > 0 ? (h*this.bins[b].value/mx).toFixed(1) : 0);
+				ha = Math.round(h-hbar);
+				hb = h-ha;
+				if(hb < 1) ha--;
+				idbar = id+'-bar-'+(typeof key==="string" ? b : key.replace(/ /g,'-'));
 				this.bins[b].id = idbar;
-				if(maketable) output += '<td id="'+idbar+'" style="width:'+(100/nbins).toFixed(3)+'%;" class="'+(!this.bins[b].selected ? ' deselected' : '')+'" data-index="'+b+'"><div class="antibar" style="height:'+ha+'px;"></div><div class="bar" title="'+key+': '+(this.attr.units || "")+this.formatNumber(this.bins[b].value)+'" style="height:'+hb+'px;"></div>'+(((typeof key==="string" && key.indexOf('-01')) || key.indexOf('-')==-1) ? '<span class="label">'+this.attr.formatKey.call(this,key)+'</span>' : '')+'</td>';
+				if(maketable) output += '<td id="'+idbar+'" style="width:'+(100/this.nbins).toFixed(3)+'%;" class="'+(!this.bins[b].selected ? ' deselected' : '')+'" data-index="'+b+'"><div class="antibar" style="height:'+ha+'px;"></div><a href="#" class="bar" title="'+key+': '+(this.attr.units || "")+this.formatNumber(this.bins[b].value)+'" style="height:'+hb+'px;"></a>'+(((typeof key==="string" && key.indexOf('-01')) || key.indexOf('-')==-1) ? '<span class="label">'+this.attr.formatKey.call(this,key)+'</span>' : '')+'</td>';
 				else{
-					var p = S('#'+idbar+'');
+					p = S('#'+idbar+'');
 					p.attr('class',''+(!this.bins[b].selected ? 'deselected' : ''))
 					p.find('.bar').css({'height':hb+'px'}).attr('title',key+': '+(this.attr.units || "")+this.formatNumber(this.bins[b].value));
 					p.find('.antibar').css({'height':ha+'px'});
@@ -202,15 +224,15 @@
 			}
 			// Attach the events
 			if(!this.drawn){
-				S(this.target+' .bar').parent()
-					.on('click',{me:this,parent:this.attr.parent},function(e){ e.data.me.trigger("barclick",{event:e}); })
+				S(this.target+' .bar')
+					.on('focus',{me:this,parent:this.attr.parent},function(e){ e.currentTarget = e.currentTarget.parentNode; e.data.me.trigger("barover",{event:e}); })
+				.parent()
+					.on('click',{me:this,parent:this.attr.parent},function(e){ e.preventDefault(); e.data.me.trigger("barclick",{event:e}); })
 					.on('mouseover',{me:this,parent:this.attr.parent},function(e){ e.data.me.trigger("barover",{event:e}); });
 			}
 			this.drawn = true;
 		}
-	
 		return this;
-
 	}
 	
 	// Attach a handler to an event for the Graph object in a style similar to that used by jQuery
@@ -254,7 +276,7 @@
 		if(!mintick) mintick = 3;
 		var t_inc = Math.pow(base,Math.floor(Math.log(rg)/Math.log(base)));
 		t_inc *= 2;
-		var t_max = (Math.floor(mx/t_inc))*t_inc;
+		var t_max = (Math.ceil(mx/t_inc))*t_inc;
 		if(t_max < mx) t_max += t_inc;
 		var t_min = t_max;
 		var i = 0;
@@ -280,8 +302,12 @@
 	
 	BarChart.prototype.formatNumber = function(v){
 		if(typeof v !== "number") return v;
-		if(v >= 1e7) return Math.round(v/1e6)+"M";
-		if(v >= 1e6) return (v/1e6).toFixed(1)+"M";
+		if(v >= 1e11) return Math.round(v/1e9)+"B";
+		if(v >= 1e10) return (v/1e9).toFixed(1).replace(/\.?0+$/,"")+"B";
+		if(v >= 1e9) return (v/1e9).toFixed(2).replace(/\.?0+$/,"")+"B";
+		if(v >= 1e8) return Math.round(v/1e6)+"M";
+		if(v >= 1e7) return (v/1e6).toFixed(1).replace(/\.0*$/,"")+"M";
+		if(v >= 1e6) return (v/1e6).toFixed(2).replace(/\.0*$/,"")+"M";
 		if(v >= 1e5) return Math.round(v/1e3)+"k";
 		if(v >= 1e4) return Math.round(v/1e3)+"k";
 		// Remove rounding issues
