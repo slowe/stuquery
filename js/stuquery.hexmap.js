3d4c2e334528057c104481c90abb64c2c7d767d3
Stuart Lowe <gravatar@strudel.org.uk>
2018-11-08 12:47:17 +0000

    Add polyfill

--- js/stuquery.hexmap.js
+++ js/stuquery.hexmap.js
@@ -17,6 +17,17 @@
 		};
 	}else G.extend = Object.extend;
 
+	// Polyfill
+	if (Element.prototype.getAttributeNames == undefined) {
+		Element.prototype.getAttributeNames = function () {
+			var attributes = this.attributes;
+			var length = attributes.length;
+			var result = new Array(length);
+			for (var i = 0; i < length; i++) result[i] = attributes[i].name;
+			
+			return result;
+		};
+	}
 
 	// Main function
 	function HexMap(el,attr){
